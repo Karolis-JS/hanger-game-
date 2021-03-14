bb1dc899264a91e04ae8649a71a862677c1934f5
unknown <karolis.soblinskas@gmail.com>
2021-03-14 14:10:29 +0200

    game ready to use

--- index.js
+++ index.js
@@ -1,25 +1,172 @@
-const head = document.getElementById('head')
-const body = document.getElementById('body')
-const lh = document.getElementById('lh')
-const rh = document.getElementById('rh')
-const legs = document.getElementById('legs')
-let input = document.getElementById('input')
-let playerBtn = document.getElementById('playerBtn')
-let letter = document.getElementsByClassName('letter')[0]
-let word = document.getElementsByClassName('word')
 
-playerBtn.addEventListener('click', checkLetter)
+(function () {
+    "use strict";
+    let availableLetters, words, guessInput, guess, guessButton, lettersGuessed, lettersMatched,
+        output, man, letters, lives, currentWord, numLettersMatched, messages;
 
-function checkLetter(){
-    for (let i = 0; i < word.length; i++) {
-        // console.log(word[i].innerText)
-        if (word[i].innerText.includes(input.value)){
-        word[i].style.display = 'block'
-        } else {
-            head.style.display = "block"
+    const head = document.getElementById("head")
+    const body = document.getElementById("body")
+    const lh = document.getElementById("lh")
+    const rh = document.getElementById("rh")
+    const legs = document.getElementById("legs")
+    const bodyParts = document.getElementsByClassName('body-part')
+
+    function setup() {
+        bodyPartsNone()
+        availableLetters = "abcdefghijklmnopqrstuvwxyz";
+        lives = 5;
+        words = ["cat", "dog", "cow", "reindeer"];
+        messages = {
+            win: 'You win!',
+            lose: 'Game over!',
+            guessed: ' already guessed, please try again...',
+            validLetter: 'Please enter a letter from A-Z'
+        };
+        //end config options
+        lettersGuessed = lettersMatched = '';
+        numLettersMatched = 0;
+
+        //choose a word
+        currentWord = words[Math.floor(Math.random() * words.length)];
+        console.log(currentWord)
+
+        //make #man and #output blank, create vars for later access
+        output = document.getElementById("output");
+        man = document.getElementById("man");
+        guessInput = document.getElementById("letter");
+
+        man.innerHTML = 'You have ' + lives + ' lives remaining';
+        output.innerHTML = '';
+
+        document.getElementById("letter").value = '';
+
+        //make sure guess button is enabled
+        guessButton = document.getElementById("guess");
+        guessInput.style.display = 'inline';
+        guessButton.style.display = 'inline';
+
+        //set up display of letters in current word
+        letters = document.getElementById("letters");
+        letters.innerHTML = '<li class="current-word">Current word:</li>';
+
+        let letter, i;
+        for (i = 0; i < currentWord.length; i++) {
+            letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
+            letters.insertAdjacentHTML('beforeend', letter);
         }
     }
 
-    console.log(word)
-    console.log(input.value)
-}
+    function gameOver(win) {
+        if (win) {
+            output.innerHTML = messages.win;
+            output.classList.add('win');
+        } else {
+            output.innerHTML = messages.lose;
+            output.classList.add('error');
+        }
+
+        guessInput.style.display = guessButton.style.display = 'none';
+        guessInput.value = '';
+    }
+
+    //Start game - should ideally check for existing functions attached to window.onload
+    window.onload = setup();
+
+    //buttons
+    document.getElementById("restart").onclick = setup;
+
+    //reset letter to guess on click
+    guessInput.onclick = function () {
+        this.value = '';
+    };
+
+    //main guess function when user clicks #guess
+    document.getElementById('hangman').onsubmit = function (e) {
+        if (e.preventDefault) e.preventDefault();
+        output.innerHTML = '';
+        output.classList.remove('error', 'warning');
+        guess = guessInput.value;
+
+        //does guess have a value? if yes continue, if no, error
+        if (guess) {
+            //is guess a valid letter? if so carry on, else error
+            if (availableLetters.indexOf(guess) > -1) {
+                // has it been guessed (missed or matched) already? if so, abandon & add notice */
+                if ((lettersMatched && lettersMatched.indexOf(guess) > -1) || (lettersGuessed && lettersGuessed.indexOf(guess) > -1)) {
+                    output.innerHTML = '"' + guess.toUpperCase() + '"' + messages.guessed;
+                    output.classList.add("warning");
+                }
+                // does guess exist in current word? if so, add to letters already matched, if final letter added, game over with win message */
+                else if (currentWord.indexOf(guess) > -1) {
+                    let lettersToShow;
+                    lettersToShow = document.querySelectorAll(".letter" + guess.toUpperCase());
+
+                    for (let i = 0; i < lettersToShow.length; i++) {
+                        lettersToShow[i].classList.add("correct");
+                    }
+
+                    //check to see if letter appears multiple times
+                    for (let j = 0; j < currentWord.length; j++) {
+                        if (currentWord.charAt(j) === guess) {
+                            numLettersMatched += 1;
+                        }
+                    }
+
+                    lettersMatched += guess;
+                    if (numLettersMatched === currentWord.length) {
+                        gameOver(true);
+                    }
+                }
+                //guess doesn't exist in current word and hasn't been guessed before, add to lettersGuessed, reduce lives & update user */
+                else {
+                    lettersGuessed += guess;
+                    lives--;
+                    man.innerHTML = 'You have ' + lives + ' lives remaining';
+                    showBodyParts()
+                    if (lives === 0) gameOver();
+                }
+            }
+            //not a valid letter, error
+            else {
+                output.classList.add('error');
+                output.innerHTML = messages.validLetter;
+            }
+        }
+        // no letter entered, error
+        else {
+            output.classList.add('error');
+            output.innerHTML = messages.validLetter;
+        }
+        return false;
+    };
+    function showBodyParts(){
+        if (lives === 4){
+            head.style.display = "block"
+        }
+        if (lives === 3){
+            body.style.display = "block"
+        }
+        if (lives === 2){
+            lh.style.display = "block"
+        }
+        if (lives === 1){
+            rh.style.display = "block"
+        }
+        if (lives === 0){
+            legs.style.display = "block"
+        }
+    }
+    function bodyPartsNone(){
+        head.style.display = "none"
+        body.style.display = "none"
+        lh.style.display = "none"
+        rh.style.display = "none"
+        legs.style.display = "none"
+    }
+
+}());
+
+// const body = document.getElementById("body")
+// const lh = document.getElementById("lh")
+// const rh = document.getElementById("rh")
+// const legs = document.getElementById("legs")
